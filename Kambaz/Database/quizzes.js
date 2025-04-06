7519b2bfe891b52d1b78efc231026736e771e2cc
zachkaufman36 <35308366+zachkaufman36@users.noreply.github.com>
2025-04-06 14:19:29 -0400

    Created some routes for quizzes

---
+++ Kambaz/Database/quizzes.js
@@ -0,0 +1,12 @@
+export default [
+    { "_id": "Q101", "title": "Propulsion Assignment", "course": "RS101", "release_date": "May 6, 2025, 12:00 AM", "due_date": "May 13, 2025, 11:59 PM", "points": "100" },
+    { "_id": "Q102", "title": "Combustion Analysis", "course": "RS101", "release_date": "May 13, 2025, 12:00 AM", "due_date": "May 20, 2025, 11:59 PM", "points": "125" },
+    { "_id": "Q103", "title": "Nozzle Design Project", "course": "RS101", "release_date": "May 20, 2025, 12:00 AM", "due_date": "May 27, 2025, 11:59 PM", "points": "75" },
+    { "_id": "Q201", "title": "Aerodynamics Quiz", "course": "RS102", "release_date": "May 6, 2025, 12:00 AM", "due_date": "May 13, 2025, 11:59 PM", "points": "100" },
+    { "_id": "Q202", "title": "Flow Analysis", "course": "RS102", "release_date": "May 13, 2025, 12:00 AM", "due_date": "May 20, 2025, 11:59 PM", "points": "125" },
+    { "_id": "Q203", "title": "Heating Analysis", "course": "RS102", "release_date": "May 20, 2025, 12:00 AM", "due_date": "May 27, 2025, 11:59 PM", "points": "75" },
+    { "_id": "Q301", "title": "Structural Design Task", "course": "RS103", "release_date": "May 6, 2025, 12:00 AM", "due_date": "May 13, 2025, 11:59 PM", "points": "100" },
+    { "_id": "Q302", "title": "Orbital Calculations", "course": "RS103", "release_date": "May 13, 2025, 12:00 AM", "due_date": "May 20, 2025, 11:59 PM", "points": "125" },
+    { "_id": "Q303", "title": "Systems Engineering Exam", "course": "RS103", "release_date": "May 20, 2025, 12:00 AM", "due_date": "May 27, 2025, 11:59 PM", "points": "75" }
+]
+  
